e52fe7918dc7866c7252dad0cb2a7e5303ec4c4d
Blinkov Ilya <blinkovilya@yandex-team.ru>
2024-08-02 17:58:50 +0300

    add basic tables into init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,41 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, firstName VARCHAR(64), lastName VARCHAR(64));
+DROP SCHEMA public CASCADE;
+
+CREATE SCHEMA public;
+
+GRANT ALL ON SCHEMA public TO postgres;
+
+GRANT ALL ON SCHEMA public TO public;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  firstName VARCHAR(64),
+  lastName VARCHAR(64)
+);
+
+CREATE TABLE friendship_requests (
+  requesterId INT REFERENCES users (id),
+  responderId INT REFERENCES users (id),
+  accepted BOOLEAN,
+  viewed BOOLEAN
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  authorId INTEGER REFERENCES users (id),
+  message VARCHAR(4096)
+);
+
+CREATE TABLE post_likes (
+  authorId INTEGER REFERENCES users (id),
+  postId INTEGER REFERENCES posts (id)
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  authorId INTEGER REFERENCES users (id)
+);
+
+CREATE TABLE comment_likes (
+  authorId INTEGER REFERENCES users (id),
+  commentId INTEGER REFERENCES comments (id)
+);
