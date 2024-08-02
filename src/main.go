096ae4cb5a0bc37d41984a7d15d88a8d9dc5e7ce
Blinkov Ilya <blinkovilya@yandex-team.ru>
2024-08-02 20:57:01 +0300

    connect server and postgres containers

--- src/main.go
+++ src/main.go
@@ -2,29 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
 )
 
 type User struct {
-	ID        string `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
+	ID        string `json:"id" db:"id"`
+	FirstName string `json:"firstName" db:"first_name"`
+	LastName  string `json:"lastName" db:"last_name"`
 }
 
 func main() {
-	fmt.Println("Server started ⚡")
+	users := []User{}
 
-	users := []User{
-		{"1", "Ilya", "Blinkov"},
-		{"2", "Ilya", "Olegblinkov"},
-		{"3", "Ilya", "Olegblinkov"},
-		{"3", "Ilya", "Olegblinkov"},
+	db, err := sqlx.Connect("pgx", "postgres://postgres:123@database:5432/nosebook")
+	if err != nil {
+		log.Fatalln(err)
 	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatalln(pingErr)
+	}
+
+	fmt.Println("Connected to postgres database!")
 
 	router := gin.Default()
 	router.GET("/", func(ctx *gin.Context) {
+		db.Select(&users, "SELECT * FROM users")
 		ctx.IndentedJSON(http.StatusOK, users)
 	})
 
