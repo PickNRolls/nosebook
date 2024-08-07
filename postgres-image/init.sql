fef5c50e05f6c84292bb6f9fbea9921e983c62c6
Blinkov Ilya <blinkovilya@yandex-team.ru>
2024-08-07 11:41:21 +0300

    add more data samples into sql init script

--- postgres-image/init.sql
+++ postgres-image/init.sql
@@ -60,10 +60,75 @@ CREATE TABLE comment_likes (
 
 -- Data samples
 INSERT INTO
-  users (first_name, last_name, nick, passhash)
+  users (id, first_name, last_name, nick, passhash)
 VALUES
-  ('Ilya', 'Blinkov', 'zzz', 'password'),
-  ('Marina', 'Graf', 'mmm', 'password'),
-  ('Sasha', 'Provodnikov', 'yyy', 'password'),
-  ('Tolber', 'Ovcharenko', 'tolber01', 'password'),
-  ('Sanal', 'Mandjiev', 'sanal', 'password');
+  (
+    '1ae02f69-ea1a-4308-b825-0e5896e652e4',
+    'Ilya',
+    'Blinkov',
+    'drugtester',
+    '$2a$04$A08tmv8hEQkc75GbpRlpMO6ClwAwEfEO0I1YG2qB56o/jsOdtn3hS'
+  ),
+  (
+    '48683858-796c-45ad-a361-9e3d6d003354',
+    'Marina',
+    'Graf',
+    'mmm',
+    '$2a$04$A08tmv8hEQkc75GbpRlpMO6ClwAwEfEO0I1YG2qB56o/jsOdtn3hS'
+  ),
+  (
+    'baa0e8bc-385f-4314-9580-29855aff2229',
+    'Sasha',
+    'Provodnikov',
+    'yyy',
+    '$2a$04$A08tmv8hEQkc75GbpRlpMO6ClwAwEfEO0I1YG2qB56o/jsOdtn3hS'
+  ),
+  (
+    '37d28fdf-99bc-44b5-8df9-6a3b1a36f177',
+    'Tolber',
+    'Ovcharenko',
+    'tolber01',
+    '$2a$04$A08tmv8hEQkc75GbpRlpMO6ClwAwEfEO0I1YG2qB56o/jsOdtn3hS'
+  ),
+  (
+    '2db640fd-7aa4-4bba-8ee6-3935b700297a',
+    'Sanal',
+    'Mandjiev',
+    'sanal',
+    '$2a$04$A08tmv8hEQkc75GbpRlpMO6ClwAwEfEO0I1YG2qB56o/jsOdtn3hS'
+  );
+
+INSERT INTO
+  comments (id, author_id, message, created_at)
+VALUES
+  (
+    '620c79b7-3927-48b7-a308-1ffd3db6036f',
+    '1ae02f69-ea1a-4308-b825-0e5896e652e4',
+    'comment message',
+    TIMESTAMP '2024-02-16 15:36:38'
+  ),
+  (
+    'd0023f4d-8d7f-4907-9438-d2ed2a9661fa',
+    '1ae02f69-ea1a-4308-b825-0e5896e652e4',
+    '2nd comment message',
+    TIMESTAMP '2024-02-16 15:36:55'
+  );
+
+INSERT INTO
+  posts (id, author_id, owner_id, message, created_at)
+VALUES
+  (
+    'c7b7bf17-38f9-4ed5-b0a8-501a90f7c8e7',
+    '1ae02f69-ea1a-4308-b825-0e5896e652e4',
+    '1ae02f69-ea1a-4308-b825-0e5896e652e4',
+    'post message',
+    TIMESTAMP '2024-02-16 14:36:38'
+  );
+
+INSERT INTO
+  post_comments (post_id, comment_id)
+VALUES
+  (
+    'c7b7bf17-38f9-4ed5-b0a8-501a90f7c8e7',
+    'd0023f4d-8d7f-4907-9438-d2ed2a9661fa'
+  );
